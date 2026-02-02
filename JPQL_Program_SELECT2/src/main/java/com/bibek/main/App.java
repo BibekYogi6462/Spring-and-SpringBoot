5836094bcfc2694b92f20c380e04d353ecb0e0f5
bibekyogi6462 <bibekyogi6462@gmail.com>
2026-02-02 18:55:56 +0545

    JPQL TypedQuery;

---
+++ JPQL_Program_SELECT2/src/main/java/com/bibek/main/App.java
@@ -0,0 +1,42 @@
+package com.bibek.main;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import com.bibek.entity.Student;
+
+public class App {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("main-persistence-unit");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            String jpqlQuery = "SELECT s FROM Student s";
+            TypedQuery<Student> query = em.createQuery(jpqlQuery,Student.class);
+
+            List<Student> stdList = query.getResultList();
+
+            for (Student s : stdList) {
+                System.out.println(
+                        s.getId() + " | " +
+                        s.getName() + " | " +
+                        s.getEmail() + " | " +
+                        s.getMarks()
+                );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
